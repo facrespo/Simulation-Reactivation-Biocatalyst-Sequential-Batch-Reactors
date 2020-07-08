de55c5f2705d9d0e03096bc5ede88bc2a625bb05
Fernando A. Crespo <facrespo@gmail.com>
2020-07-08 13:44:05 -0300

    Add files via upload

--- simulation_without_activation.py
+++ simulation_without_activation.py
@@ -10,10 +10,10 @@ import reactorbioquim;
 
 km=10;
 kcat=1;
-kd=0.03;
+kd=0.022;
 e0=18;
 Si=200;
-Xopt=0.9;
+Xopt=0.1;
 nlotes=6;
 kr=0.5;
 V0=1;
@@ -52,4 +52,30 @@ plt.plot(x[1:nlotes],product[1:nlotes]);
 plt.xlabel('lotes');
 plt.ylabel('producto')
 plt.title('Producto')
+plt.show();
+
+curva_without_activation=np.zeros((10,nlotes+2),dtype=float);
+for i in range(0,nlotes):
+    if (i==0):
+        curva_without_activation[0:10,0:3] = reactorbioquim.curva(km, kcat, kd, e0_st[i], Si, V0, Mcat);
+    else:
+        temp=reactorbioquim.curva(km, kcat, kd, e0_st[i], Si, V0, Mcat);
+        curva_without_activation[0:10,i+2] =temp[0:10,2];
+
+fich2=open("plot_curve_whithout_activation.txt","w");
+fich2.write("Point,Xopt,");
+for p in range(0,nlotes):
+    fich2.write("lotes_" +str(p+1) + ",");
+fich2.write("\n");
+for i in range(0,10):
+   for p in range(0,nlotes+2):
+      fich2.write(str(curva_without_activation[i,p]) + ",");
+   fich2.write("\n");  
+
+fich2.close();
+
+plt.plot(curva_without_activation[0:9,2],curva_without_activation[0:9,1]);
+plt.xlabel('time');
+plt.ylabel('Activity')
+plt.title('Plot of Activity verus time')
 plt.show();
