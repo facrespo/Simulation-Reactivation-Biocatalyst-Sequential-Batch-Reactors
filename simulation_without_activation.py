df812d3a979cabcb16ca1dd4ddfeb5ebbdc98456
Fernando A. Crespo <facrespo@gmail.com>
2020-07-10 21:40:23 -0300

    Add files via upload

--- simulation_without_activation.py
+++ simulation_without_activation.py
@@ -54,13 +54,17 @@ plt.ylabel('producto')
 plt.title('Producto')
 plt.show();
 
+e1 = e0_st[0];
 curva_without_activation=np.zeros((10,nlotes+2),dtype=float);
 for i in range(0,nlotes):
     if (i==0):
-        curva_without_activation[0:10,0:3] = reactorbioquim.curva(km, kcat, kd, e0_st[i], Si, V0, Mcat);
+        curva_without_activation[0:10,0:3] = reactorbioquim.curva(km, kcat, kd, e1, Si, V0, Mcat);
+        e1=e1*np.exp(-kd*curva_without_activation[9,2]);
     else:
-        temp=reactorbioquim.curva(km, kcat, kd, e0_st[i], Si, V0, Mcat);
-        curva_without_activation[0:10,i+2] =temp[0:10,2];
+        if (e1 >= elimit*e0_st[0]):
+           temp=reactorbioquim.curva(km, kcat, kd, e1, Si, V0, Mcat);
+           curva_without_activation[0:10,i+2] =temp[0:10,2];
+           e1=e1*np.exp(-kd*temp[9,2]);
 
 fich2=open("plot_curve_whithout_activation.txt","w");
 fich2.write("Point,Xopt,");
