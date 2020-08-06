a561651ec7b1d3873946a69af2a87e2bee26608a
Fernando A. Crespo <facrespo@gmail.com>
2020-08-06 17:16:06 -0400

    Add files via upload

--- simulacion_reactors_withandwithout_activation_random.py
+++ simulacion_reactors_withandwithout_activation_random.py
@@ -18,7 +18,7 @@ kd=np.random.uniform(0.021,1.307,naleatorios);
 e0=np.random.normal(18,0,naleatorios);
 Si=np.random.normal(200,0,naleatorios);
 Xopt=0.9;
-beta=np.random.normal(0.95,0,naleatorios);
+beta=np.random.normal(0.95,0.85,naleatorios);
 kr=np.random.uniform(0.1,0.37,naleatorios);
 V0=np.random.normal(1,0,naleatorios);
 Mcat=np.random.normal(1,0,naleatorios);
@@ -26,7 +26,7 @@ dbeta=0.1;
 xlimit=0.95;
 elimit=0.25;
 
-fich=open("resultadosimulacionactivada_n"  +str(nlotes) + ".txt","w");
+fich=open("resultadosimulacionactivated_n"  +str(nlotes) + ".txt","w");
 fich.write("Model,km,kcat,kd,e0,Si,Xopt,nlotes,beta,kr,V0,Mcat,");
 for p in range(0,nlotes+1):
     fich.write("e0_st" +str(p) + ",");
@@ -46,7 +46,7 @@ fich.write("Productividad_especifica_global");
 fich.write("\n");  
 fich.close();
 
-fich2=open("resultadosimulacionsin_n"  +str(nlotes) + ".txt","w");
+fich2=open("resultadosimulacionwithoutactivate_n"  +str(nlotes) + ".txt","w");
 fich2.write("Model,km,kcat,kd,e0,Si,Xopt,nlotes,V0,Mcat,");
 for p in range(0,nlotes+1):
     fich2.write("e0_st" +str(p) + ",");
@@ -63,8 +63,8 @@ fich2.close();
 
 
 for i in range(0,naleatorios):
-    e0_st, Gamma, e_ref_actividad, t_reaccion, t_reactivacion, product, productivity_specif, Productividad_especifica_global  = reactorbioquim.reaccionactivada(km[i], kcat[i], kd[i], e0[i], Si[i], Xopt, nlotes, beta[i], kr[i], V0[i], Mcat[i], dbeta, xlimit, elimit);
-    fich=open("resultadosimulacionactivada_n"  +str(nlotes) + ".txt","a");
+    e0_st, betaf, Gamma, e_ref_actividad, t_reaccion, t_reactivacion, product, productivity_specif, Productividad_especifica_global  = reactorbioquim.reaccionactivada(km[i], kcat[i], kd[i], e0[i], Si[i], Xopt, nlotes, beta[i], kr[i], V0[i], Mcat[i], dbeta, xlimit, elimit);
+    fich=open("resultadosimulacionactivated_n"  +str(nlotes) + ".txt","a");
     fich.write(str(i) + "," + str(km[i]) + "," + str(kcat[i]) + "," + str(kd[i]) + "," + str(e0[i]) + "," + str(Si[i]) + "," + str(Xopt) + "," + str(nlotes) + "," + str(beta[i]) + ","  + str(kr[i]) + "," + str(V0[i]) + "," + str(Mcat[i]) + ",");
     for p in range(0,nlotes+1):
         fich.write(str(e0_st[p]) + ",");
@@ -84,7 +84,7 @@ for i in range(0,naleatorios):
     fich.write("\n");  
     fich.close();
     e0_st, t_reaccion, product, productivity_specif, Productividad_especifica_global = reactorbioquim.reaccion(km[i], kcat[i], kd[i], e0[i], Si[i], Xopt, nlotes, V0[i], Mcat[i], elimit);
-    fich2=open("resultadosimulacionsin_n"  +str(nlotes) + ".txt","a");
+    fich2=open("resultadosimulacionwithoutactivate_n"  +str(nlotes) + ".txt","a");
     fich2.write(str(i) + "," + str(km[i]) + "," + str(kcat[i]) + "," + str(kd[i]) + "," + str(e0[i]) + "," + str(Si[i]) + "," + str(Xopt) + "," + str(nlotes) + "," +  str(V0[i]) + "," + str(Mcat[i]) + "," );
     for p in range(0,nlotes+1):
         fich2.write(str(e0_st[p]) + ",");
