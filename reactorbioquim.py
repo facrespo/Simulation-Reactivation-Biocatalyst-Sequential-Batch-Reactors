c2174ef099e36692d8ee9218619a932d8383317d
Fernando A. Crespo <facrespo@gmail.com>
2020-08-12 21:43:28 -0400

    Add files via upload

--- reactorbioquim.py
+++ reactorbioquim.py
@@ -1,8 +1,6 @@
 import numpy as np;
-import csv;
 import scipy;
 from scipy import stats;
-import matplotlib.pyplot as plt;
 from scipy.optimize import fsolve, root, broyden1;
 from sympy import Symbol,nsolve;
 
@@ -37,17 +35,17 @@ def solvereactor(Xopt, Si, km, kcat, kd, e0):
     resolucion=scipy.optimize.fsolve(reactor,x0,args=(Xopt, Si, km, kcat, kd, e0))
     return resolucion;
 
-def solveactividad(Eopt, beta, gamma, kr, e0):
+def solveactivity(Eopt, beta, gamma, kr, e0):
     #x0=0;
     x0=0;
     resolucion=scipy.optimize.fsolve(eactividad,x0,args=(beta, gamma, kr, e0, Eopt))
     #resolucion=-(1/kr)*np.log((Eopt-beta*e0)/(e0*(gamma-beta)));
     return resolucion;
 
-def reaccionactivada(km, kcat, kd, e0, Si, Xopt, nlotes, beta, kr, V0, Mcat, dbeta, xlimit, elimit):
-    e0_st=np.zeros(nlotes+1,dtype=float);
+def reactivity(km, kcat, kd, e0, Si, Xopt, nlotes, beta, kr, V0, Mcat, dbeta, xlimit, elimit):
+    efinish=np.zeros(nlotes+1,dtype=float);
     betat=np.zeros(nlotes+1,dtype=float);
-    e_ref_actividad=np.zeros(nlotes+1,dtype=float);
+    e0_activity=np.zeros(nlotes+1,dtype=float);
     Gamma=np.zeros(nlotes+1,dtype=float);
     t_reaccion=np.zeros(nlotes+1,dtype=float);
     t_reactivacion=np.zeros(nlotes+1,dtype=float);
@@ -58,8 +56,8 @@ def reaccionactivada(km, kcat, kd, e0, Si, Xopt, nlotes, beta, kr, V0, Mcat, dbe
     betat[0]=0;
     betat[1]=1;
     betat[2]=beta;
-    e0_st[0]=e0;
-    e_ref_actividad[0]=e0;
+    efinish[0]=e0;
+    e0_activity[0]=e0;
     Gamma[0]=1;
     t_reactivacion[0]=0;
     t_reaccion[0]=0;
@@ -71,26 +69,26 @@ def reaccionactivada(km, kcat, kd, e0, Si, Xopt, nlotes, beta, kr, V0, Mcat, dbe
     Productividadr=0;
     for i in range(1,nlotes+1):
         en=e0;
-        if (i>=3) and (e0_st[i-1] >= elimit*e0_st[0]):
+        if (i>=3) and (efinish[i-1] >= elimit*efinish[0]):
            betat[i]=betat[i-1]-dbeta;
            if (betat[i]<=0):
                betat[i]=0;
-        if (e0_st[i-1] >= elimit*e0_st[0]):
+        if (efinish[i-1] >= elimit*efinish[0]):
            if (i==1):
-               e_ref_actividad[i]=e0_st[0];
+               e0_activity[i]=efinish[0];
            else:
-               e_ref_actividad[i]=xlimit*e0_st[0]*betat[i];
+               e0_activity[i]=xlimit*efinish[0]*betat[i];
            if (i==1):
-               t_reaccion[i] = solvereactor(Xopt, Si, km, kcat, kd, e_ref_actividad[i-1]);
+               t_reaccion[i] = solvereactor(Xopt, Si, km, kcat, kd, e0_activity[i-1]);
            else:
-               t_reaccion[i] = solvereactor(Xopt, Si, km, kcat, kd, e_ref_actividad[i]);
+               t_reaccion[i] = solvereactor(Xopt, Si, km, kcat, kd, e0_activity[i]);
            if t_reaccion[i]>0:
-              e0_st[i]=e_ref_actividad[i]*np.exp(-kd*t_reaccion[i]);
-              Gamma[i]=e0_st[i]/e0_st[0];
+              efinish[i]=e0_activity[i]*np.exp(-kd*t_reaccion[i]);
+              Gamma[i]=efinish[i]/efinish[0];
               if (i==1):
                   t_reactivacion[i]=0;
               else:
-                  t_reactivacion[i]=solveactividad(e_ref_actividad[i], betat[i], Gamma[i], kr, e0_st[0]);
+                  t_reactivacion[i]=solveactivity(e0_activity[i], betat[i], Gamma[i], kr, efinish[0]);
               tiempo=tiempo+ t_reaccion[i]+t_reactivacion[i];
               X[i]=Xopt;
               V[i]=V0;
@@ -98,17 +96,17 @@ def reaccionactivada(km, kcat, kd, e0, Si, Xopt, nlotes, beta, kr, V0, Mcat, dbe
               productivity_specif[i]=product[i]/(t_reaccion[i]+t_reactivacion[i]);
        
     Productividad_especifica_global=sum(product)/(tiempo);
-    return e0_st, betat, Gamma, e_ref_actividad, t_reaccion, t_reactivacion, product, productivity_specif, Productividad_especifica_global;
+    return efinish, betat, Gamma, e0_activity, t_reaccion, t_reactivacion, product, productivity_specif, Productividad_especifica_global;
 
-def reaccion(km, kcat, kd, e0, Si, Xopt, nlotes, V0, Mcat, elimit):
-    e0_st=np.zeros(nlotes+1,dtype=float);
+def inactivity(km, kcat, kd, e0, Si, Xopt, nlotes, V0, Mcat, elimit):
+    efinish=np.zeros(nlotes+1,dtype=float);
     e_ref_actividad=np.zeros(nlotes+1,dtype=float);
     t_reaccion=np.zeros(nlotes+1,dtype=float);
     V=np.zeros(nlotes+1,dtype=float);
     X=np.zeros(nlotes+1,dtype=float);
     product=np.zeros(nlotes+1,dtype=float);
     productivity_specif=np.zeros(nlotes+1,dtype=float);
-    e0_st[0]=e0;
+    efinish[0]=e0;
     t_reaccion[0]=0;
     V[0]=V0;
     X[0]=1;
@@ -118,10 +116,10 @@ def reaccion(km, kcat, kd, e0, Si, Xopt, nlotes, V0, Mcat, elimit):
     Productividadr=0;
     for i in range(1,nlotes+1):
         en=e0;
-        if (i>=1) and (e0_st[i-1] >= elimit*e0_st[0]):
-           t_reaccion[i] = solvereactor(Xopt, Si, km, kcat, kd, e0_st[i-1]);
+        if (i>=1) and (efinish[i-1] >= elimit*efinish[0]):
+           t_reaccion[i] = solvereactor(Xopt, Si, km, kcat, kd, efinish[i-1]);
            if t_reaccion[i]>0:
-               e0_st[i]=e0_st[i-1]*np.exp(-kd*t_reaccion[i]);
+               efinish[i]=efinish[i-1]*np.exp(-kd*t_reaccion[i]);
                tiempo=tiempo+ t_reaccion[i];
                X[i]=Xopt;
                V[i]=V0;
@@ -137,9 +135,9 @@ def reaccion(km, kcat, kd, e0, Si, Xopt, nlotes, V0, Mcat, elimit):
                productivity_specif[i]=0;
        
     Productividad_especifica_global=sum(product)/(tiempo);
-    return e0_st, t_reaccion, product, productivity_specif, Productividad_especifica_global;
+    return efinish, t_reaccion, product, productivity_specif, Productividad_especifica_global;
 
-def curva(km, kcat, kd, e0, Si, V0, Mcat):
+def curv(km, kcat, kd, e0, Si, V0, Mcat):
     Xopt=np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], dtype=float);
     proyeccion=np.zeros((10,3),dtype=float);
     proyeccion[0,0]=1;
@@ -150,7 +148,7 @@ def curva(km, kcat, kd, e0, Si, V0, Mcat):
                    
     return proyeccion;
 
-def curva_activada(km, kcat, kd, e0, e0f, eref, Si, beta, kr, V0, Mcat, gamma, nlote, xlimit):
+def curv_activity(km, kcat, kd, e0, e0f, eref, Si, beta, kr, V0, Mcat, gamma, nlote, xlimit):
     Xopt=np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], dtype=float);
     proyeccion=np.zeros((10,4),dtype=float);
     proyeccion[0,0]=1;
@@ -166,7 +164,7 @@ def curva_activada(km, kcat, kd, e0, e0f, eref, Si, beta, kr, V0, Mcat, gamma, n
            if (nlote==1):
                t2=0;
            else:
-               t2=solveactividad(eref, beta, gamma, kr, e0);
+               t2=solveactivity(eref, beta, gamma, kr, e0);
            if (t2<=0):
                t2=0;
         proyeccion[i,2]=t1;
